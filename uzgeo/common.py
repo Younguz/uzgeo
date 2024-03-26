436f45450df708c815497eba13008ce92890b9ac
Yuze Li <yli190@vols.utk.edu>
2024-03-25 21:41:31 -0400

    adding functions 1

--- uzgeo/common.py
+++ uzgeo/common.py
@@ -5,3 +5,28 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+
+def calculate_average(sequence):
+    """
+    Calculate the average value of a sequence.
+
+    Args:
+        sequence (list or tuple): The sequence of numbers.
+
+    Returns:
+        float: The average value of the sequence.
+    """
+    if not sequence:
+        return None  # Return None for empty sequence
+
+    total = sum(sequence)
+    average = total / len(sequence)
+    return average
+
+# Given sequence
+number = (1, 5, 5, 6, 7)
+
+# Calculating the mean
+mean_value = calculate_average(number)
+print("Mean:", mean_value)
