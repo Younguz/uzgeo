c5b3fb427f17c659ef1eeffe91e7f82512eb3ce1
Younguz <158337690+Younguz@users.noreply.github.com>
2024-03-26 00:44:18 -0400

    Update uzgeo.py

--- uzgeo/uzgeo.py
+++ uzgeo/uzgeo.py
@@ -3,7 +3,24 @@
 import ipyleaflet
 
 class Map(ipyleaflet.Map):
+    """
+    Calculate the average value of a sequence.  
 
+    Args:
+    sequence (list or tuple): The sequence of numbers.
+
+    Returns:
+    float: The average value of the sequence.
+    """
     def __init__(self, center=[20,0], zoom=2, **kwargs):
+        """
+        Calculate the average value of a sequence.  
+
+        Args:
+        sequence (list or tuple): The sequence of numbers.
+
+        Returns:
+        float: The average value of the sequence.
+        """
         super().__init__(center=center, zoom=zoom, **kwargs) 
         self.add_control(ipyleaflet.LayersControl())
