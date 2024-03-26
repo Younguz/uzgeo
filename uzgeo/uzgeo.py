1f741a4a6818af95216ce5ea751e925f2ae45d59
Yuze Li <yli190@vols.utk.edu>
2024-03-26 00:59:50 -0400

    lab6111

--- uzgeo/uzgeo.py
+++ uzgeo/uzgeo.py
@@ -4,23 +4,23 @@ import ipyleaflet
 
 class Map(ipyleaflet.Map):
     """
-    Calculate the average value of a sequence.  
+    This is the map class that inherits from ipyleaflet.Map.
 
     Args:
-    sequence (list or tuple): The sequence of numbers.
+        sequence (list or tuple): The sequence of numbers.
 
     Returns:
-    float: The average value of the sequence.
+        float: The average value of the sequence.
     """
     def __init__(self, center=[20,0], zoom=2, **kwargs):
         """
         Calculate the average value of a sequence.  
 
         Args:
-        sequence (list or tuple): The sequence of numbers.
+            sequence (list or tuple): The sequence of numbers.
 
         Returns:
-        float: The average value of the sequence.
+            float: The average value of the sequence.
         """
         super().__init__(center=center, zoom=zoom, **kwargs) 
         self.add_control(ipyleaflet.LayersControl())
